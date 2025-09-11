39c668d6440622cd4e7fd9922d5e18126c03e132
“윤재홍” <a287933@naver.com>
2025-09-12 05:36:36 +0900

    feat(index.js):함수통합 밎 class구현

--- src/main.js
+++ src/main.js
@@ -13,54 +13,61 @@ function makeRandomNumber() {
   return numbers;
 }
 
-document.getElementById("result").style.display = "none";
-
-const computerInputNumbers = makeRandomNumber();
-
-document.getElementById('submit').addEventListener('click', (event) => {
-  event.preventDefault();
-  const userInput = document.getElementById('user-input').value;
-
-  if (!isValidInput(userInput)) {
-    alert("잘못된 입력입니다. 중복되지 않는 서로 다른 3개의 숫자를 입력하세요");
-    document.getElementById("user-input").value = "";
-    return;
+export default class BaseballGame {
+  constructor() {
+    this.computerInputNumbers = makeRandomNumber();
+    this.initEventListeners();
   }
 
-  const userInputNumbers = numToArr(userInput);
+  initEventListeners() {
+    document.getElementById("submit").addEventListener("click", this.handleSubmit.bind(this));
+  }
 
-  const resultString = play(computerInputNumbers, userInputNumbers);
-  changeHtml(resultString);
-})
+  play(computerInputNumbers, userInputNumbers) {
+    let strike = 0;
+    let ball = 0;
 
-function play(computerInputNumbers, userInputNumbers) {
-  let strike = 0;
-  let ball = 0;
-
-  for (let i = 0; i < computerInputNumbers.length; i++) {
-    if (computerInputNumbers[i] === userInputNumbers[i]) {
-      strike++;
-    } else if (computerInputNumbers.includes(userInputNumbers[i])) {
-      ball++;
+    for (let i = 0; i < computerInputNumbers.length; i++) {
+      if (computerInputNumbers[i] === userInputNumbers[i]) {
+        strike++;
+      } else if (computerInputNumbers.includes(userInputNumbers[i])) {
+        ball++;
+      }
     }
+    return this.getPlayResultString(strike, ball);
   }
-  return getPlayResultString(strike, ball);
-};
 
-function getPlayResultString(strike, ball) {
-  if (strike === 0 && ball === 0) {
-    return "낫싱";
-  }
-  let result = "";
-  if (ball > 0) {
-    result += `${ball}볼`;
-  }
-  if (strike > 0) {
-    if (result) {
-      result += " ";
+  getPlayResultString(strike, ball) {
+    if (strike === 0 && ball === 0) {
+      return "낫싱";
     }
-    result += `${strike}스트라이크`;
+    let result = "";
+    if (ball > 0) {
+      result += `${ball}볼`;
+    }
+    if (strike > 0) {
+      if (result) {
+        result += " ";
+      }
+      result += `${strike}스트라이크`;
+    }
+    return result;
   }
 
-  return result;
-};
+  handleSubmit(e) {
+    e.preventDefault();
+    const userInput = document.getElementById("user-input").value;
+
+    if (!isValidInput(userInput)) {
+      alert("잘못된 입력입니다. 중복되지 않는 서로 다른 3개의 숫자를 입력하세요");
+      document.getElementById("user-input").value = "";
+      return;
+    }
+
+    const userInputNumbers = numToArr(userInput);
+    const resultString = this.play(this.computerInputNumbers, userInputNumbers);
+    changeHtml(resultString);
+  }
+}
+
+new BaseballGame();
