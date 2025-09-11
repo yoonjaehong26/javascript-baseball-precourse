b1bf4369440330612a3735087e4ac7953b556058
“윤재홍” <a287933@naver.com>
2025-09-12 03:01:33 +0900

    feat(index.js) : 입력데이터 저장

--- src/main.js
+++ src/main.js
@@ -12,4 +12,5 @@ function makeRandomNumber() {
 let userInputNumbers;
 document.getElementById('submit').addEventListener('click', (event) => {
   event.preventDefault();
+  userInputNumbers = document.getElementById('user-input').value;
 });
