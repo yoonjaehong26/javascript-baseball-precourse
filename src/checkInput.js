a69c2f68f5e15233f16a0d9cbf00cc37b39ee0c1
“윤재홍” <a287933@naver.com>
2025-09-12 03:44:06 +0900

    feat(checkInput.js):배열중복검사

--- src/checkInput.js
+++ src/checkInput.js
@@ -1,10 +1,16 @@
 import utils from './util.js';
 
+function hasDuplicate(input) {
+  const uniqueNumbers = new Set(input);
+  return uniqueNumbers.size !== input.length;
+}
 
 export default function isValidInput(input) {
   if (input.length !== 3) {
     return false;
   }
-  
+  if (hasDuplicate(input)) {
+    return false;
+  }
   return true;
 }
