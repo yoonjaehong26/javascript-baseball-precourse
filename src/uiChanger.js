e8aca5ee264d11eb7c1e4c9bb1b035d944751a60
“윤재홍” <a287933@naver.com>
2025-09-16 15:57:16 +0900

    fix(uiChanger.js):이전 ui 변경관련 커밋에서 반영 안된 부분 수정
    이전 5a2de4fc7aa2800a6cbb6a9c4ab63e7dbbfd02cb커밋에서
     index.js getPlayResultString함수에서 바꾼 결과를 changeGameResultUI함수에서 반영이 안된 부분 수정

--- src/uiChanger.js
+++ src/uiChanger.js
@@ -1,7 +1,7 @@
 export function changeGameResultUI(resultString) {
   document.getElementById("result").style.display = "block";
   document.getElementById("result").innerHTML = resultString;
-  document.getElementById("game-restart-button").style.display = resultString === "3스트라이크" ? "block" : "none";
+  document.getElementById("game-restart-button").style.display = resultString === "🎉축하합니다!!!🎉 정답을 맞추셨습니다. <br> 재시작 하시겠습니까?" ? "block" : "none";
 }
 
 export function resetGameResultUI() {
