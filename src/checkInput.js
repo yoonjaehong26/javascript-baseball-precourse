cfe6ffcaad9d63d704f9d40839ee4fca6d4b08f0
yoonjaehong26 <yjh0202261@gmail.com>
2025-09-17 01:49:56 +0900

    [1주차] 숫자 야구게임 미션 제출합니다. (#32)
    * docs(README) : 기능 목록 추가
    
    * feat(index.js) : 유효한 랜덤변수 생성
    
    * feat(index.html,index.js) : 입력데이터 리스너생성
    
    * feat(index.js) : 입력데이터 저장
    
    * feat(checkInput.js):배열길이검사
    
    * feat(checkInput.js):배열중복검사
    
    * feat(checkInput.js):배열값타당성검사
    
    * fix(index.js,checkInput.js):import오류수정
    
    import대상범위 오류로 인해 import대상변경
    
    * feat(index.js):입력데이터 유효성검사
    
    * feat(index.js):야구게임 로직구현
    
    * feat(index.js, uiChanger.js):ui변경
    
    * feat(index.js):함수통합 밎 class구현
    
    * fix(iddex.js,index.html):파일import오류수정
    
    * feat(index.js):재시작기능구현
    
    재시작기능이 없어 재시작시 변수재설정기능을 추가
    
    * feat(index.js,uiChanger.js):재시작 ui초기화
    
    * refacter(index.js,checkInput.js):데이터 자리수 소프트코드
    
    코드 확장성을 위하여 숫자야구데이터의 자리수를 3에서 변수로 변경
    
    * refacter(index.js,uiChanger.js):불명확한 함수명 변경
    
    가독성과 통일성이 떨어지는 함수이름 변경:changeHrml=>changeUI,initEventListeners=>uiEventListeners,handlesummit=>gameLogic
    
    * refactor(index.js):함수명 통일성 준수
    
     함수명의 통일일성을 위해 함수명 변경함 gmaeLogic-유지,  handleRestart=>restartLogic
    
    * fix(index.js):restartLogic 함수명 정정
    
    이전 커밋에서 바꾸지 못한 함수명 정정 uiEventListeners내
    handleRestart=>restartLogic
    
    * feat(index.js)함수문법 가독성개선
    
    함수.bind(this)문법 가독성 감소하여 화살표함수로 대체
    
    * refactor(index.js):변수 가독성을 위한 변수명 변경
    
    변수 타입과 명확성의 부족으로 다음과 같이 변경
    ball=>ballcount
    strike=>strikecount
    
    전체적은 변수명 통일을 위하여 다음과 같이 변수명 통일
    result=>resultstring
    
    * feat(index.js) 3스트라이크 시 메세지를 수정하여 게임완료를 명확하게 알림
    
    * refactor(index.js): airbnb 코드 컨벤션에 따른 연산자 표현 변경
    
    * refactor(index.js,uiChanger.js):함수목적성을 강조하기 위한 함수명 변경
    
    * refactor(checkInput.js):불필요한 numToArr함수 import삭제
    
    * refactor(index.js):에어비엔비 코드 컨벤션에 따른 연산자 좌우 빈칸 확보
    
    * feat(checkInput.js):입력 유효성 검사 정규표현식 활용 조건 추가
    
    * refactor(checkInput.js):중복된 숫자검증 제거
    
    * fix(uiChanger.js):이전 ui 변경관련 커밋에서 반영 안된 부분 수정
    
    이전 5a2de4fc7aa2800a6cbb6a9c4ab63e7dbbfd02cb커밋에서
     index.js getPlayResultString함수에서 바꾼 결과를 changeGameResultUI함수에서 반영이 안된 부분 수정
    
    ---------
    
    Co-authored-by: “윤재홍” <“a287933@naver.com”>

---
+++ src/checkInput.js
@@ -0,0 +1,25 @@
+function hasDuplicate(input) {
+  const uniqueNumbers = new Set(input);
+  return uniqueNumbers.size !== input.length;
+}
+
+function isEachNumberInRange(input) {
+  for (const char of input) {
+    const number = parseInt(char, 10);
+    if (isNaN(number) || number < 1 || number > 9) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export default function isValidInput(input, digitCount) {
+  if (input.length !== digitCount) {
+    return false;
+  }
+  if (hasDuplicate(input)) {
+    return false;
+  }
+
+  return isEachNumberInRange(input);
+}
