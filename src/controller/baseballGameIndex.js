52edefc06e8e9a831c7d06e01d9f96a639d25982
윤재홍 <a287933@naver.com>
2025-09-19 16:48:10 +0900

    refactor(/src,index.html):MVC패턴 적용
    model,view,controller폴더 생성 후 각 역할에 맞는 함수 위치 변경,할당
    view,model의 경우 클라스 문법을 사용하여 코드의 모듈화에 집중함
    전체적인 코드의 구조변화 리펙터링임에 주의하여 코드구조변경 외의 다른 기능추가나
    다른 영역 리펙터링을 하지 않음

---
+++ src/controller/baseballGameIndex.js
@@ -0,0 +1,44 @@
+import BaseballGameModel from '../model/BaseballGameModel.js';
+import BaseballGameView from '../view/BaseballGameView.js';
+
+import isValidInput from '../utils/checkInput.js';
+import numToArr from '../utils/additionalUtilFunction.js';
+
+export default class BaseballGame {
+  constructor(digitCount = 3) {
+    this.digitCount = digitCount;
+    this.BaseballGameModel = new BaseballGameModel(this.digitCount);
+    this.BaseballGameView = new BaseballGameView();
+
+    this.computerInputNumbers = this.BaseballGameModel.makeRandomNumber();
+    this.uiEventListeners();
+    this.BaseballGameView.resetGameResultUI();
+  }
+
+  uiEventListeners() {
+    document.getElementById('submit').addEventListener('click', this.gameLogic);
+    document.getElementById('game-restart-button').addEventListener('click', this.restartLogic);
+  }
+
+  gameLogic = e => {
+    e.preventDefault();
+
+    const userInput = document.getElementById('user-input').value;
+    if (!isValidInput(userInput, this.digitCount)) {
+      this.BaseballGameView.alertWrongUserInput()
+      return;
+    }
+
+    const userInputNumbers = numToArr(userInput);
+    const resultString = this.BaseballGameModel.play(this.computerInputNumbers, userInputNumbers);
+
+    this.BaseballGameView.changeGameResultUI(resultString);
+  };
+
+  restartLogic = () => {
+    this.computerInputNumbers = this.BaseballGameModel.makeRandomNumber();
+    this.BaseballGameView.resetGameResultUI();
+  };
+}
+
+const game = new BaseballGame();
